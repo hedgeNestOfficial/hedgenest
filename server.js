62e2bc18f749263411c282326e39cbfa7dd75252
abolarinwachristianah2024-code <abolarinwachristianah2024@gmail.com>
2026-06-02 16:11:04 +0100

    creation of swagger documentation for all the user endpoint, kyc and wallet

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 3333
 const app = express()
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(expressSession({
   secret:'michael',
