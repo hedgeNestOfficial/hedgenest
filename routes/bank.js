2c6e5cbb7765d13269ecabbedb11b692e64b9f78
abolarinwachristianah2024-code <abolarinwachristianah2024@gmail.com>
2026-06-13 16:47:44 +0100

    bank updated

--- routes/bank.js
+++ routes/bank.js
@@ -3,6 +3,6 @@ const { getBankCode, linkBank } = require('../controller/bank')
 const {Authentication} = require('../middleware/auth')
 const{bankDetailsValidator} = require('../middleware/validators')
 
-router.post('/getCode', Authentication, bankDetailsValidator, linkBank);
+router.post('/linkBank', Authentication, bankDetailsValidator, linkBank);
 
 module.exports = router
