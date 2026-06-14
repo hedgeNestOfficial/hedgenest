b4ef25fbfff2922857411770f4a6e392ca2b5939
abolarinwachristianah2024-code <abolarinwachristianah2024@gmail.com>
2026-06-14 23:56:31 +0100

    logout endpoint has been created

--- controller/user.js
+++ controller/user.js
@@ -487,16 +487,16 @@ exports.changePin = async (req, res) => {
   }
 };
 
-// exports.logout = async (req, res) => {
-//     try {
-//         return res.status(200).json({
-//             status: true,
-//             message: "Logged out successfully"
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             status: false,
-//             message: error.message
-//         });
-//     }
-// };
+exports.logout = async (req, res) => {
+    try {
+        return res.status(200).json({
+            status: true,
+            message: "Logged out successfully"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+};
