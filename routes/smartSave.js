ccc61b8a0775800b519f423440afd1099d20d450
iyanu9491-netizen <iyanu9491@gmail.com>
2026-06-22 19:46:31 +0100

    done

--- routes/smartSave.js
+++ routes/smartSave.js
@@ -1,9 +1,10 @@
 const router = require ('express').Router()
-const {previewPlan, createPlan, breakPlan, topUpFlexible, getAllPlan, getOnePlan, getUserWithPlan, getUserWithPlans} = require ('../controller/smartSave')
-const {Authentication} = require ('../middleware/auth')
+const {previewPlan, createPlan, breakPlan, topUpFlexible, getAllPlan, getOnePlan, getUserWithPlan, getUserWithPlans, getPreviewPlan} = require ('../controller/smartSave')
+const {Authentication} = require ('../middleware/auth');
+const { createPlanValidator } = require('../middleware/validators');
 
-router.get('/preview-plan', Authentication, previewPlan);
-router.post('/create-plan', Authentication, createPlan)
+router.post('/preview-plan', Authentication, previewPlan);
+router.post('/create-plan',createPlanValidator, Authentication, createPlan)
 router.post('/break-plan/:planId', Authentication, breakPlan)
 router.post('/top-up/:savingId', Authentication, topUpFlexible)
 
@@ -11,4 +12,5 @@ router.get('/get-all-plan', Authentication, getAllPlan)
 router.get('/get-one-plan/:id',Authentication,  getOnePlan)
 router.get('/get-user-plan/:userId/:planId',Authentication, getUserWithPlan)
 router.get('/get-user-with-all-plan', Authentication, getUserWithPlans)
+router.get('/get-preview-plan/:planId', Authentication, getPreviewPlan )
 module.exports = router
